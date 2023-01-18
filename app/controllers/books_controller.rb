5e3fc8bf0602afaa08bfdacf9b1f79ed50a2e415
Threeestone <ec2-user@ip-172-31-5-135.ap-northeast-1.compute.internal>
2023-01-18 12:03:56 +0000

    [add]1/18

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-     redirect_to book_path
+     redirect_to book_path(@book.id)
     else
      render :new
     end
@@ -34,6 +34,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :opinion)
+    params.require(:book).permit(:title, :body)
   end
 end
